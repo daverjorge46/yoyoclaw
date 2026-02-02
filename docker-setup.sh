c3399ee78390e137e5bca8b8372482202249303f
esteve-luzia <esteve@luzia.com>
2026-02-02 14:50:55 +0100

    Update docker-setup.sh

--- docker-setup.sh
+++ docker-setup.sh
@@ -150,7 +150,7 @@ upsert_env() {
   fi
 
   for k in "${keys[@]}"; do
-    if [[ "$seen" != *"|$k|"* ]]; then
+    if ! grep -Fq "|$k|" <<< "$seen"; then
       printf '%s=%s\n' "$k" "${!k-}" >>"$tmp"
     fi
   done
